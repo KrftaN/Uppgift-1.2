ba072e5c05c9fe8eeabacdb11ee04b6a900f5d2a
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-06 10:30:03 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ändra bakgrundsfärg till mörkblå
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            // Ändra textfärgen till vit
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Någonting gick fel. Pröva starta om datorn.");
+        }
+    }
+}
